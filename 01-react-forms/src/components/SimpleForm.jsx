844508e4e853cc8a53089f502901992bde53930a
Francisco Campos Diaz <franciscocamposdiaz@Franciscos-MacBook-Air.local>
2022-11-23 19:37:25 -0600

    Simple Form

--- 01-react-forms/src/components/SimpleForm.jsx
+++ 01-react-forms/src/components/SimpleForm.jsx
@@ -6,18 +6,31 @@ const SimpleForm = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
+  const handleSubmit = (event) => {
+    // Evito que se recargue la pagina y rompa el SPA
+    event.preventDefault()
+    const submittedData = JSON.stringify(
+      {
+        email,
+        password
+      })
+    console.log(submittedData)
+  }
   // Paso 2: Crear el formulario base en JSX
   return (
     <div className='login'>
       <div className='login-container'>
         <img src={logo} alt='logo' />
         <form>
-
+          {/* Paso 3: Guardar cada cambio del input en su estado */}
           <label htmlFor='email'>Email</label>
-          <input type='text' name='email' placeholder='correo@mail.com' id='email' />
+          <input type='text' name='email' placeholder='correo@mail.com' id='email' onChange={(event) => setEmail(event.target.value)} />
 
           <label htmlFor='password'>Password</label>
-          <input type='password' name='password' id='password' />
+          <input type='password' name='password' id='password' onChange={(event) => setPassword(event.target.value)} />
+          <button onClick={handleSubmit}>
+            Iniciar Sesion
+          </button>
         </form>
       </div>
     </div>
