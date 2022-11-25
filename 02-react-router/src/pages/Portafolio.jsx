8548b7fd7a852cc141650e2952cd903a86610d5a
César Guerra <warderer@gmail.com>
2022-11-24 21:25:37 -0600

    ✨ Routeo Dinámico y Anidado con useParams y Outlet

--- 02-react-router/src/pages/Portafolio.jsx
+++ 02-react-router/src/pages/Portafolio.jsx
@@ -1,6 +1,27 @@
+import { Link, Outlet } from 'react-router-dom'
+/* <Outlet /> es un componente que sirve en ruteos anidados para
+indicarle al Padre, donde debe renderearse el hijo */
+
 const Portafolio = () => {
   return (
-    <h1>Portafolio</h1>
+    <>
+      <h1>Portafolio</h1>
+      <Outlet />
+      <nav>
+        <ul>
+          <li>
+            <Link to='/portafolio/1'>Pinterest</Link>
+          </li>
+          <li>
+            <Link to='/portafolio/2'>Cajero</Link>
+          </li>
+          <li>
+            <Link to='/portafolio/3'>Pokédex</Link>
+          </li>
+        </ul>
+      </nav>
+
+    </>
   )
 }
 export default Portafolio
