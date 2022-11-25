520007864e789aeb4807ad1bc192b1f6ed436627
César Guerra <warderer@gmail.com>
2022-11-24 19:45:17 -0600

    🎉 react router project init

---
+++ 02-react-router/src/App.jsx
@@ -0,0 +1,12 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App () {
+  return (
+    <div className='App'>
+      <h1>App</h1>
+    </div>
+  )
+}
+
+export default App
