c92e26bcdc4c9374e7ef8b3fc649a07eeb8e029d
Francisco Campos Diaz <franciscocamposdiaz@Franciscos-MacBook-Air.local>
2022-11-28 21:28:20 -0600

    Refactor de componentes para usar el contexto

--- 03-react-context/src/components/SongDetails/SongDetails.jsx
+++ 03-react-context/src/components/SongDetails/SongDetails.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './songdetails.css'
+import { useSongContext } from '@/context/SongContext'
 
 const SongDetails = () => {
+  const context = useSongContext()
   return (
-    <div>SongDetails</div>
+    <>
+      {
+        context.selectedSong.title
+          ? <div>
+            <img src={`https://picsum.photos/seed/${context.selectedSong.id}/400/400`} alt='Portada de; Disco' />
+            <h1>{context.selectedSong.title}</h1>
+            <h4>{context.selectedSong.artist}</h4>
+            <h6>{context.selectedSong.year}</h6>
+            </div>
+          : <h1>Selecciona una cancion</h1>
+      }
+    </>
   )
 }
 
