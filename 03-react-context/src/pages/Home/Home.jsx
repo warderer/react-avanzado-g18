49c735ef310b4df572613966d550ae2309e4131d
Francisco Campos Diaz <franciscocamposdiaz@Franciscos-MacBook-Air.local>
2022-11-28 20:27:33 -0600

    pages home

--- 03-react-context/src/pages/Home/Home.jsx
+++ 03-react-context/src/pages/Home/Home.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import './home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home-container'>
+      <div className='izquierdo'>
+        <h2>Lado Izquierdo</h2>
+      </div>
+      <div className='derecho'>
+        <h2>Lado Derecho</h2>
+      </div>
+    </div>
   )
 }
 
