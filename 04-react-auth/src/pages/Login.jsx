624ad1094c9f9b57dac6abad12cc412f55783f76
César Guerra <warderer@gmail.com>
2022-12-05 21:03:31 -0600

    💄 Signup y Login UI

--- 04-react-auth/src/pages/Login.jsx
+++ 04-react-auth/src/pages/Login.jsx
@@ -10,12 +10,28 @@ const Login = () => {
           <h1 className='h3 mb-3 fw-normal'>Please sign in</h1>
 
           <div className='form-floating'>
-            <input type='email' className='form-control' id='floatingInput' placeholder='name@example.com' />
-            <label htmlFor='floatingInput'>Email address</label>
+            <input
+              type='email'
+              className='form-control'
+              id='email'
+              placeholder='name@example.com'
+              name='email'
+              value=''
+              onChange={() => {}}
+            />
+            <label htmlFor='email'>Email address</label>
           </div>
           <div className='form-floating'>
-            <input type='password' className='form-control' id='floatingPassword' placeholder='Password' />
-            <label htmlFor='floatingPassword'>Password</label>
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              placeholder='Password'
+              name='password'
+              value=''
+              onChange={() => {}}
+            />
+            <label htmlFor='password'>Password</label>
           </div>
 
           <button className='w-100 btn btn-lg btn-primary' type='submit'>Sign in</button>
--- 04-react-auth/src/pages/Signup.jsx
+++ 04-react-auth/src/pages/Signup.jsx
@@ -1,6 +1,93 @@
+import '@/assets/css/form.css'
+import logo from '@/assets/react.svg'
+
 const Signup = () => {
+//   {
+//     "first_name": "Danilo",
+//     "last_name": "Parc",
+//     "gender": "M",
+//     "email": "danylo@gmail.com",
+//     "password": "gatito123"
+// }
   return (
-    <h1>Signup</h1>
+    <div>
+      <main className='form-signin w-100 m-auto'>
+        <form>
+          <img className='mb-4' src={logo} alt='' width='72' height='57' />
+          <h1 className='h3 mb-3 fw-normal'>Please sign up</h1>
+
+          <div className='form-floating'>
+            <input
+              type='text'
+              className='form-control'
+              id='first_name'
+              placeholder='Elias'
+              name='first_name'
+              value=''
+              onChange={() => {}}
+            />
+            <label htmlFor='first_name'>First Name</label>
+          </div>
+
+          <div className='form-floating'>
+            <input
+              type='text'
+              className='form-control'
+              id='last_name'
+              placeholder='Shuchleib'
+              name='last_name'
+              value=''
+              onChange={() => {}}
+            />
+            <label htmlFor='last_name'>Last Name</label>
+          </div>
+
+          <div className='form-floating'>
+            <select
+              className='form-select'
+              id='gender'
+              name='gender'
+              value=''
+              onChange={() => {}}
+            >
+              <option value=''>Choose...</option>
+              <option value='M'>Male</option>
+              <option value='F'>Female</option>
+            </select>
+            <label htmlFor='gender'>Gender</label>
+          </div>
+
+          <div className='form-floating'>
+            <input
+              type='email'
+              className='form-control'
+              id='email'
+              placeholder='name@example.com'
+              name='email'
+              value=''
+              onChange={() => {}}
+            />
+            <label htmlFor='email'>Email address</label>
+          </div>
+
+          <div className='form-floating'>
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              placeholder='Password'
+              name='password'
+              value=''
+              onChange={() => {}}
+            />
+            <label htmlFor='password'>Password</label>
+          </div>
+
+          <button className='w-100 btn btn-lg btn-primary' type='submit'>Sign in</button>
+          <p className='mt-5 mb-3 text-muted'>© 2017–2022</p>
+        </form>
+      </main>
+    </div>
   )
 }
 export default Signup
